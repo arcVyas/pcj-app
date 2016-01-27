09f43f3fb075ba2a32497487bbd672faa1f0721a
Vyas <vyas.mohan@gmail.com>
2016-01-27 01:42:03 -0600

    cleaner version

--- src/main/groovy/com/jcp/poc/Controller.groovy
+++ src/main/groovy/com/jcp/poc/Controller.groovy
@@ -4,8 +4,11 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.CrossOrigin
+import com.jcp.poc.beans.Coupon
 
-@Controller
+@RestController
 class GreetingController {
 
     @RequestMapping("/greeting")
@@ -13,5 +16,18 @@ class GreetingController {
         model.addAttribute("name", name);
         return "greeting";
     }
-
+    
+    @CrossOrigin(origins = "http://localhost:9080")
+    @RequestMapping(value = "/coupons", produces = "application/json")
+    Coupon[] getCoupons() {
+      return [
+        new Coupon(
+          offerType: "IN STORE",
+          offerText: "30% OFF IN STORE",
+          offerDetails: "select orig and reg-priced apparel, shoes, accessories, fine jewelry & fashion jewelry",
+          offerConditions: "VALID THROUGH JANUARY 30, 2016, NO CODE NECESSARY",
+          offerGroup: "STOREWIDE & SITEWIDE OFFERS"
+        )
+      ]
+    }
 }
---
+++ src/main/groovy/com/jcp/poc/beans/coupon.groovy
@@ -0,0 +1,9 @@
+package com.jcp.poc.beans
+
+class Coupon{
+  String offerType
+  String offerText
+  String offerDetails
+  String offerConditions
+  String offerGroup
+} 
